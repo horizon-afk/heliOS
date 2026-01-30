6bdc3b021c839f90055aafa5f1e4c948dfc39eb3
Umesh <umeshsarkar19@outlook.com>
2026-01-30 18:55:34 +0530

    Added paging

--- kernel.c
+++ kernel.c
@@ -3,6 +3,28 @@
 
 extern char __bss[], __bss_end[], __stack_top[];
 
+void *memset(void *buf, char c, size_t n)
+{
+    uint8_t *p = (uint8_t *)buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n)
+{
+    static paddr_t next_paddr = (paddr_t)__free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t)__free_ram_end)
+        PANIC("out of memory");
+
+    memset((void *)paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
 __attribute__((naked))
 __attribute__((aligned(4)))
 void kernel_entry(void){
