1b06a00810ddb5ab1695e8935ec74274dbf8325d
Umesh <umeshsarkar19@outlook.com>
2026-02-12 01:42:09 +0530

    Added process and scheduling

--- kernel.c
+++ kernel.c
@@ -25,11 +25,131 @@ paddr_t alloc_pages(uint32_t n)
     return paddr;
 }
 
+struct process procs[PROCS_MAX];
+
+struct process *create_process(uint32_t pc)
+{
+  
+    struct process *proc = NULL;
+    int i;
+    for (i = 0; i < PROCS_MAX; i++)
+    {
+        if (procs[i].state == PROC_UNUSED)
+        {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+        PANIC("no free process slots");
+
+    
+    uint32_t *sp = (uint32_t *)&proc->stack[sizeof(proc->stack)];
+    *--sp = 0; 
+    *--sp = 0; 
+    *--sp = 0;
+    *--sp = 0;
+    *--sp = 0;
+    *--sp = 0;
+    *--sp = 0;            
+    *--sp = 0;           
+    *--sp = 0;           
+    *--sp = 0;            
+    *--sp = 0;            
+    *--sp = 0;            
+    *--sp = (uint32_t)pc; 
+
+    
+    proc->pid = i + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t)sp;
+    return proc;
+}
+
+__attribute__((naked)) void switch_context(uint32_t *prev_sp,
+                                           uint32_t *next_sp) {
+    __asm__ __volatile__(
+        "addi sp, sp, -13 * 4\n" 
+        "sw ra,  0  * 4(sp)\n" 
+        "sw s0,  1  * 4(sp)\n"
+        "sw s1,  2  * 4(sp)\n"
+        "sw s2,  3  * 4(sp)\n"
+        "sw s3,  4  * 4(sp)\n"
+        "sw s4,  5  * 4(sp)\n"
+        "sw s5,  6  * 4(sp)\n"
+        "sw s6,  7  * 4(sp)\n"
+        "sw s7,  8  * 4(sp)\n"
+        "sw s8,  9  * 4(sp)\n"
+        "sw s9,  10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+
+        
+        "sw sp, (a0)\n"         
+        "lw sp, (a1)\n"
+
+        "lw ra,  0  * 4(sp)\n"  
+        "lw s0,  1  * 4(sp)\n"
+        "lw s1,  2  * 4(sp)\n"
+        "lw s2,  3  * 4(sp)\n"
+        "lw s3,  4  * 4(sp)\n"
+        "lw s4,  5  * 4(sp)\n"
+        "lw s5,  6  * 4(sp)\n"
+        "lw s6,  7  * 4(sp)\n"
+        "lw s7,  8  * 4(sp)\n"
+        "lw s8,  9  * 4(sp)\n"
+        "lw s9,  10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n"
+        "ret\n"
+    );
+}
+
+struct process *current_proc; // Currently running process
+struct process *idle_proc;    // Idle process
+
+void yield(void)
+{
+   
+    struct process *next = idle_proc;
+    for (int i = 0; i < PROCS_MAX; i++)
+    {
+        struct process *proc = &procs[(current_proc->pid + i) % PROCS_MAX];
+        if (proc->state == PROC_RUNNABLE && proc->pid > 0)
+        {
+            next = proc;
+            break;
+        }
+    }
+
+
+    if (next == current_proc)
+        return;
+
+    __asm__ __volatile__(
+        "csrw sscratch, %[sscratch]\n"
+        :
+        : [sscratch] "r"((uint32_t)&next->stack[sizeof(next->stack)]));
+
+    __asm__ __volatile__(
+        "csrw sscratch, %[sscratch]\n"
+        :
+        : [sscratch] "r"((uint32_t)&next->stack[sizeof(next->stack)]));
+
+    // Context switch
+    struct process *prev = current_proc;
+    current_proc = next;
+    switch_context(&prev->sp, &next->sp);
+}
+
 __attribute__((naked))
 __attribute__((aligned(4)))
 void kernel_entry(void){
     __asm__ __volatile__(
-        "csrw sscratch, sp\n"
+        "csrrw sp, sscratch, sp\n"
+
         "addi sp, sp, -4 * 31\n"
         "sw ra,  4 * 0(sp)\n"
         "sw gp,  4 * 1(sp)\n"
@@ -64,6 +184,9 @@ void kernel_entry(void){
 
         "csrr a0, sscratch\n"
         "sw a0, 4 * 30(sp)\n"
+
+        "addi a0, sp, 4 * 31\n"
+        "csrw sscratch, a0\n"
 
         "mv a0, sp\n"
         "call handle_trap\n"
@@ -134,17 +257,47 @@ void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1);
 }
 
-void kernel_main(void) {
-    char text[] = "This is HeliOS\n";
-    printf("Hello", "World");
-    printf("%s", text);
+void delay(void)
+{
+    for (int i = 0; i < 30000000; i++)
+        __asm__ __volatile__("nop"); // do nothing
+}
 
-    WRITE_CSR(stvec, (uint32_t)kernel_entry); 
-    __asm__ __volatile__("unimp");            
+struct process *proc_a;
+struct process *proc_b;
 
-    for (;;) {
-        __asm__ __volatile__("wfi");
+void proc_a_entry(void)
+{
+    printf("starting process A\n");
+    while (1)
+    {
+        putchar('A');
+        yield();
     }
+}
+
+void proc_b_entry(void){
+
+    printf("starting process B\n");
+    while (1)
+    {
+        putchar('B');
+        yield();
+    }
+}
+
+void kernel_main(void) {
+    memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+    idle_proc = create_process((uint32_t)NULL);
+    idle_proc->pid = 0; // idle
+    current_proc = idle_proc;
+
+    proc_a = create_process((uint32_t)proc_a_entry);
+    proc_b = create_process((uint32_t)proc_b_entry);
+    yield();
+    PANIC("Swtiched to idle process!");
 }
 
 __attribute__((section(".text.boot")))
